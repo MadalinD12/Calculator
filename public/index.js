7ac0dbf197348f003109bf37b9c8fe7aaccf98ed
MadalinD12 <Dinumadalin351@yahoo.ro>
2024-12-31 16:07:07 +0200

    add deleteOne, modify style

--- public/index.js
+++ public/index.js
@@ -5,9 +5,14 @@ function appendToDisplay(input){
 }
 
 function clearDisplay(){
-    display.value="";
+    display.value = "";
 }
 
 function calculate(){
-    display.value =eval(display.value);
+    display.value = eval(display.value);
+}
+
+function deleteOne(){
+    display.value = Math.floor(display.value / 10);
+
 }
